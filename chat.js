0fcbcecc75e30762726a8994e23bdfa61958888c
wslaurance <wslaurance@missionfocus.com>
2013-08-14 13:15:12 -0400

    init that rainy day chat.js file

---
+++ chat.js
@@ -0,0 +1,22 @@
+var net = require('net'),
+    clients = [],
+    _ = require('underscore');
+
+net.createServer(function(socket){
+  if (clients.indexOf(socket) === -1){
+    clients.push(socket);
+  }
+  socket.on('data', function(d){
+    var nonMe = _.filter(clients, function(e){
+      return e !== socket;
+    });
+    _.each(nonMe, function(e){
+      e.write(d.toString());
+    });
+  });
+  socket.on('close', function(s){
+    console.log(s);
+  });
+}).listen(8080);
+
+
